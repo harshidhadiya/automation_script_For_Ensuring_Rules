0de873d783b7974583f5c8f64763072be8b988ae
harshidhadiya <harshid.hadiya@marutitech.com>
2026-04-13 18:37:57 +0530

    small change for understanding

--- Program.cs
+++ Program.cs
@@ -7,25 +7,29 @@ IConfiguration config = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile("config.json", optional: false, reloadOnChange: true)
     .Build();
-
-Console.WriteLine("Enter filter (0,1,3,4,5,6,7,30,60,90,180,custom):");
+Console.WriteLine();
+Console.WriteLine("Enter DAYS [ 0 , 1 , 3 , 4 , 5 , 6 , 7 , 30 , 60 , 90 , 180 ] This Will Return Last Days { Your Enter Day } Data");
+Console.WriteLine();
+Console.WriteLine("Write => * <= for the Enter DateRange Wise Data ");
 var input = Console.ReadLine();
 
-if (!Helper.IsValidNumber(input) && input != "custom")
+if (!Helper.IsValidNumber(input) && input != "custom" && input != "*")
 {
     Console.WriteLine("==> Invalid input. Only numbers or 'custom' allowed.");
     return;
 }
 
+
 string startDate = null;
 string endDate = null;
 
-if (input == "custom")
+if (input == "custom" || input=="*")
 {
-    Console.WriteLine("Enter start date (yyyy-MM-dd):");
+    input="custom";
+    Console.WriteLine("Enter start date ( yyyy-MM-dd ):");
     startDate = Console.ReadLine();
 
-    Console.WriteLine("Enter end date (yyyy-MM-dd) optional:");
+    Console.WriteLine("Enter end date ( yyyy-MM-dd ) optional:");
     endDate = Console.ReadLine();
 
     if (!string.IsNullOrWhiteSpace(startDate) && !Helper.DateRegex.IsMatch(startDate))
@@ -56,7 +60,6 @@ if (input == "custom")
         }
     }
 }
-
-
+Console.WriteLine(input);
 var runner = new BugAuditRunner(config);
 await runner.RunAsync(input,startDate,endDate);
